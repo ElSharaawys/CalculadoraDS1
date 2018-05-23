4da5a9d408d391fc483c05a4b684ba2104e6ee3b
Bruno Fineza <37422687+ElSharaawys@users.noreply.github.com>
2018-05-22 21:50:06 -0300

    Prova de DS

---
+++ Calcu/Calcu/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace Calcu
+{
+    partial class frmCalcu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txbResultado = new System.Windows.Forms.TextBox();
+            this.lbl1 = new System.Windows.Forms.Button();
+            this.lbl2 = new System.Windows.Forms.Button();
+            this.lbl3 = new System.Windows.Forms.Button();
+            this.lbl4 = new System.Windows.Forms.Button();
+            this.lbl5 = new System.Windows.Forms.Button();
+            this.lbl6 = new System.Windows.Forms.Button();
+            this.lbl7 = new System.Windows.Forms.Button();
+            this.lbl8 = new System.Windows.Forms.Button();
+            this.lbl9 = new System.Windows.Forms.Button();
+            this.lbl0 = new System.Windows.Forms.Button();
+            this.lblSoma = new System.Windows.Forms.Button();
+            this.lblSub = new System.Windows.Forms.Button();
+            this.lblMult = new System.Windows.Forms.Button();
+            this.lblDiv = new System.Windows.Forms.Button();
+            this.lblIgual = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txbResultado
+            // 
+            this.txbResultado.Location = new System.Drawing.Point(12, 22);
+            this.txbResultado.Name = "txbResultado";
+            this.txbResultado.Size = new System.Drawing.Size(230, 20);
+            this.txbResultado.TabIndex = 0;
+            // 
+            // lbl1
+            // 
+            this.lbl1.Location = new System.Drawing.Point(27, 92);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(40, 34);
+            this.lbl1.TabIndex = 1;
+            this.lbl1.Text = "1";
+            this.lbl1.UseVisualStyleBackColor = true;
+            this.lbl1.Click += new System.EventHandler(this.lbl1_Click);
+            // 
+            // lbl2
+            // 
+            this.lbl2.Location = new System.Drawing.Point(85, 91);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(40, 35);
+            this.lbl2.TabIndex = 2;
+            this.lbl2.Text = "2";
+            this.lbl2.UseVisualStyleBackColor = true;
+            this.lbl2.Click += new System.EventHandler(this.lbl2_Click);
+            // 
+            // lbl3
+            // 
+            this.lbl3.Location = new System.Drawing.Point(144, 91);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(40, 34);
+            this.lbl3.TabIndex = 3;
+            this.lbl3.Text = "3";
+            this.lbl3.UseVisualStyleBackColor = true;
+            this.lbl3.Click += new System.EventHandler(this.lbl3_Click);
+            // 
+            // lbl4
+            // 
+            this.lbl4.Location = new System.Drawing.Point(27, 132);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(40, 34);
+            this.lbl4.TabIndex = 4;
+            this.lbl4.Text = "4";
+            this.lbl4.UseVisualStyleBackColor = true;
+            this.lbl4.Click += new System.EventHandler(this.lbl4_Click);
+            // 
+            // lbl5
+            // 
+            this.lbl5.Location = new System.Drawing.Point(85, 132);
+            this.lbl5.Name = "lbl5";
+            this.lbl5.Size = new System.Drawing.Size(40, 34);
+            this.lbl5.TabIndex = 5;
+            this.lbl5.Text = "5";
+            this.lbl5.UseVisualStyleBackColor = true;
+            this.lbl5.Click += new System.EventHandler(this.lbl5_Click);
+            // 
+            // lbl6
+            // 
+            this.lbl6.Location = new System.Drawing.Point(144, 131);
+            this.lbl6.Name = "lbl6";
+            this.lbl6.Size = new System.Drawing.Size(40, 34);
+            this.lbl6.TabIndex = 6;
+            this.lbl6.Text = "6";
+            this.lbl6.UseVisualStyleBackColor = true;
+            this.lbl6.Click += new System.EventHandler(this.lbl6_Click);
+            // 
+            // lbl7
+            // 
+            this.lbl7.Location = new System.Drawing.Point(27, 172);
+            this.lbl7.Name = "lbl7";
+            this.lbl7.Size = new System.Drawing.Size(40, 36);
+            this.lbl7.TabIndex = 7;
+            this.lbl7.Text = "7";
+            this.lbl7.UseVisualStyleBackColor = true;
+            this.lbl7.Click += new System.EventHandler(this.lbl7_Click);
+            // 
+            // lbl8
+            // 
+            this.lbl8.Location = new System.Drawing.Point(84, 172);
+            this.lbl8.Name = "lbl8";
+            this.lbl8.Size = new System.Drawing.Size(40, 34);
+            this.lbl8.TabIndex = 8;
+            this.lbl8.Text = "8";
+            this.lbl8.UseVisualStyleBackColor = true;
+            this.lbl8.Click += new System.EventHandler(this.lbl8_Click);
+            // 
+            // lbl9
+            // 
+            this.lbl9.Location = new System.Drawing.Point(144, 174);
+            this.lbl9.Name = "lbl9";
+            this.lbl9.Size = new System.Drawing.Size(40, 34);
+            this.lbl9.TabIndex = 9;
+            this.lbl9.Text = "9";
+            this.lbl9.UseVisualStyleBackColor = true;
+            this.lbl9.Click += new System.EventHandler(this.lbl9_Click);
+            // 
+            // lbl0
+            // 
+            this.lbl0.Location = new System.Drawing.Point(27, 214);
+            this.lbl0.Name = "lbl0";
+            this.lbl0.Size = new System.Drawing.Size(97, 34);
+            this.lbl0.TabIndex = 10;
+            this.lbl0.Text = "0";
+            this.lbl0.UseVisualStyleBackColor = true;
+            this.lbl0.Click += new System.EventHandler(this.lbl0_Click);
+            // 
+            // lblSoma
+            // 
+            this.lblSoma.Location = new System.Drawing.Point(203, 91);
+            this.lblSoma.Name = "lblSoma";
+            this.lblSoma.Size = new System.Drawing.Size(37, 33);
+            this.lblSoma.TabIndex = 11;
+            this.lblSoma.Text = "+";
+            this.lblSoma.UseVisualStyleBackColor = true;
+            this.lblSoma.Click += new System.EventHandler(this.lblSoma_Click);
+            // 
+            // lblSub
+            // 
+            this.lblSub.Location = new System.Drawing.Point(202, 131);
+            this.lblSub.Name = "lblSub";
+            this.lblSub.Size = new System.Drawing.Size(38, 34);
+            this.lblSub.TabIndex = 12;
+            this.lblSub.Text = "-";
+            this.lblSub.UseVisualStyleBackColor = true;
+            this.lblSub.Click += new System.EventHandler(this.lblSub_Click);
+            // 
+            // lblMult
+            // 
+            this.lblMult.Location = new System.Drawing.Point(202, 173);
+            this.lblMult.Name = "lblMult";
+            this.lblMult.Size = new System.Drawing.Size(38, 34);
+            this.lblMult.TabIndex = 13;
+            this.lblMult.Text = "x";
+            this.lblMult.UseVisualStyleBackColor = true;
+            this.lblMult.Click += new System.EventHandler(this.lblMult_Click);
+            // 
+            // lblDiv
+            // 
+            this.lblDiv.Location = new System.Drawing.Point(203, 215);
+            this.lblDiv.Name = "lblDiv";
+            this.lblDiv.Size = new System.Drawing.Size(38, 34);
+            this.lblDiv.TabIndex = 14;
+            this.lblDiv.Text = "/";
+            this.lblDiv.UseVisualStyleBackColor = true;
+            this.lblDiv.Click += new System.EventHandler(this.lblDiv_Click);
+            // 
+            // lblIgual
+            // 
+            this.lblIgual.Location = new System.Drawing.Point(144, 217);
+            this.lblIgual.Name = "lblIgual";
+            this.lblIgual.Size = new System.Drawing.Size(40, 31);
+            this.lblIgual.TabIndex = 15;
+            this.lblIgual.Text = "=";
+            this.lblIgual.UseVisualStyleBackColor = true;
+            this.lblIgual.Click += new System.EventHandler(this.lblIgual_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(24, 59);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 13);
+            this.lblResult.TabIndex = 16;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(202, 59);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(40, 26);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "C";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // frmCalcu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(254, 260);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.lblIgual);
+            this.Controls.Add(this.lblDiv);
+            this.Controls.Add(this.lblMult);
+            this.Controls.Add(this.lblSub);
+            this.Controls.Add(this.lblSoma);
+            this.Controls.Add(this.lbl0);
+            this.Controls.Add(this.lbl9);
+            this.Controls.Add(this.lbl8);
+            this.Controls.Add(this.lbl7);
+            this.Controls.Add(this.lbl6);
+            this.Controls.Add(this.lbl5);
+            this.Controls.Add(this.lbl4);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.txbResultado);
+            this.Name = "frmCalcu";
+            this.Text = "Calculadora";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txbResultado;
+        private System.Windows.Forms.Button lbl1;
+        private System.Windows.Forms.Button lbl2;
+        private System.Windows.Forms.Button lbl3;
+        private System.Windows.Forms.Button lbl4;
+        private System.Windows.Forms.Button lbl5;
+        private System.Windows.Forms.Button lbl6;
+        private System.Windows.Forms.Button lbl7;
+        private System.Windows.Forms.Button lbl8;
+        private System.Windows.Forms.Button lbl9;
+        private System.Windows.Forms.Button lbl0;
+        private System.Windows.Forms.Button lblSoma;
+        private System.Windows.Forms.Button lblSub;
+        private System.Windows.Forms.Button lblMult;
+        private System.Windows.Forms.Button lblDiv;
+        private System.Windows.Forms.Button lblIgual;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Calcu/Calcu/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Calcu
+{
+    
+    public partial class frmCalcu : Form
+    {
+        Double numero = 0;
+        String operacao = "";
+        double valor1 = 0;
+
+
+        public frmCalcu()
+        {
+            InitializeComponent();
+        }
+
+        private void lbl1_Click(object sender, EventArgs e)
+        {
+            txbResultado.Text += "1";
+            lblResult.Text = lblResult.Text + 1;
+        }
+
+        private void lbl2_Click(object sender, EventArgs e)
+        {
+            txbResultado.Text += "2";
+            lblResult.Text = lblResult.Text + 2;
+        }
+
+        private void lbl3_Click(object sender, EventArgs e)
+        {
+            txbResultado.Text += "3" ;
+            lblResult.Text = lblResult.Text + 3;
+        }
+
+        private void lbl4_Click(object sender, EventArgs e)
+        {
+            txbResultado.Text += "4";
+            lblResult.Text = lblResult.Text + 4;
+        }
+
+        private void lbl5_Click(object sender, EventArgs e)
+        {
+            txbResultado.Text += "5";
+            lblResult.Text = lblResult.Text + 5;
+        }
+
+        private void lbl6_Click(object sender, EventArgs e)
+        {
+            txbResultado.Text += "6";
+            lblResult.Text = lblResult.Text + 6;
+        }
+
+        private void lbl7_Click(object sender, EventArgs e)
+        {
+            txbResultado.Text += "7";
+            lblResult.Text = lblResult.Text + 7;
+        }
+
+        private void lbl8_Click(object sender, EventArgs e)
+        {
+            txbResultado.Text += "8";
+            lblResult.Text = lblResult.Text + 8;
+        }
+
+        private void lbl9_Click(object sender, EventArgs e)
+        {
+            txbResultado.Text += "9";
+            lblResult.Text = lblResult.Text + 9;
+        }
+
+        private void lbl0_Click(object sender, EventArgs e)
+        {
+            txbResultado.Text += "0";
+            lblResult.Text = lblResult.Text + 0;
+        }
+
+        private void lblSoma_Click(object sender, EventArgs e)
+        {
+
+            lblIgual.PerformClick(); // ele recebe o que esta na textBox
+            operacao = "+";
+            txbResultado.Clear();
+            lblResult.Text = lblResult.Text + "+";
+                
+     
+        }
+
+     
+        private void lblSub_Click(object sender, EventArgs e)
+        {
+
+            lblIgual.PerformClick();// 
+            operacao = "-";
+            txbResultado.Clear();
+            lblResult.Text = lblResult.Text + "-";
+     
+                
+                
+            
+        }
+
+        private void lblMult_Click(object sender, EventArgs e)
+        {
+
+            lblIgual.PerformClick();
+            operacao = "*";
+            txbResultado.Clear();
+            lblResult.Text = lblResult.Text + "*";
+                 
+        }
+
+        private void lblDiv_Click(object sender, EventArgs e)
+        {
+
+            lblIgual.PerformClick(); 
+            operacao = "/";
+            txbResultado.Clear();
+            lblResult.Text = lblResult.Text + "/";
+                 
+        }
+
+        private void lblIgual_Click(object sender, EventArgs e)
+        {
+
+
+
+
+
+            switch(operacao){
+
+                case "+":
+                txbResultado.Text = (valor1 + Double.Parse(txbResultado.Text)).ToString(); // Pega o 1° valor e soma com o outro valor que está na TextBox
+            break;
+               
+
+          
+                case "-":
+                txbResultado.Text = (valor1 - Double.Parse(txbResultado.Text)).ToString();
+             break;
+
+
+            
+                case "*":
+                txbResultado.Text = (valor1 * Double.Parse(txbResultado.Text)).ToString();
+               break;
+
+
+           
+                case "/":
+                txbResultado.Text = (valor1 / Double.Parse(txbResultado.Text)).ToString();
+              break;
+
+
+
+            }
+
+            valor1 = Double.Parse(txbResultado.Text); //Armazena o resultado no valor1
+            operacao = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            numero = 0;
+            operacao = "";
+            valor1 = 0;
+            txbResultado.Text = "";
+            lblResult.Text = "";
+
+        }
+        }
+        
+
+
+    }
+
